77efbff5bfa75825a962c27b6631e48607361429
Alex <82574450+alexstevovich@users.noreply.github.com>
2021-11-15 02:50:53 -0500

    commit1

--- git/gitpush website-buddy.sh
+++ git/gitpush website-buddy.sh
@@ -34,7 +34,7 @@ fi
 
 git add --all
 git commit -m $commit
-git push https://github.com/$user_id/$repo_id.git main
+git push https://$user_id@github.com/$user_id/$repo_id.git main
 
 echo ""
 read -p "All Done! Press any key to close ..."
